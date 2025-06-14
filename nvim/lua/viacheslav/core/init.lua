4e7c0b6741e68df9c6e585536967282da23bdc28
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-06-14 23:50:15 +0200

    aaa

--- nvim/lua/viacheslav/core/init.lua
+++ nvim/lua/viacheslav/core/init.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function()
         vim.highlight.on_yank({
             higroup = 'IncSearch',
-            timeout = 40,
+            timeout = 200,
         })
     end,
 })
--- nvim/lua/viacheslav/plugins/lualine.lua
+++ nvim/lua/viacheslav/plugins/lualine.lua
@@ -23,9 +23,11 @@ return {
                 disabled_filetypes = {}
             },
             sections = {
-                lualine_a = { 'mode' },
+                lualine_a = {
+                    -- 'mode'
+                },
                 lualine_b = { branch_config },
-                lualine_c = { 'buffers' }, -- changed from filename_config to 'buffers'
+                lualine_c = { 'buffers' },
                 lualine_x = { diagnostics_config, 'filetype' },
                 lualine_y = {},
                 lualine_z = {}
