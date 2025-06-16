182c0186493131bf8bac42a5f82ba883d84279ec
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-06-16 18:50:54 +0200

    colortheme

--- nvim/lua/viacheslav/plugins/lualine.lua
+++ nvim/lua/viacheslav/plugins/lualine.lua
@@ -3,27 +3,51 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         require("lualine").setup {
+            options = {
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
+            },
             sections = {
                 lualine_a = {
                     'branch',
                 },
                 lualine_b = {
-
+                    {
+                        'buffers',
+                        mode = 0, -- 0: name, 1: index, 2: name+index, 3: number, 4: name+number
+                        max_length = function() return vim.o.columns * 2 / 3 end,
+                        symbols = {
+                            modified = ' ●',
+                            alternate_file = '',
+                            directory = '',
+                        },
+                        filetype_names = {
+                            TelescopePrompt = '',
+                            dashboard = '',
+                            packer = '',
+                            fzf = '',
+                            ['neo-tree'] = '',
+                            alpha = ''
+                        },
+                        disabled_buftypes = {
+                            'quickfix',
+                            'prompt'
+                        },
+                    }
                 },
                 lualine_c = {
-                    'buffers',
                 },
                 lualine_x = {
                     'diagnostics',
                 },
                 lualine_y = {
-                    -- 'filetype'
+
                 },
                 lualine_z = {
 
                 }
             },
-            extensions = { 'fugitive' }
+            extensions = { 'fugitive' },
         }
     end
 }
