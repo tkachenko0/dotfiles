d50c8af27eb58660a9f1d0dedef7a4714b1f44d7
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-12-09 23:52:09 +0100

    feat(harpoon): added harpoon

--- modules/zsh/install.sh
+++ modules/zsh/install.sh
@@ -12,6 +12,10 @@ sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 chsh -s "$(which zsh)"
 
+# Install plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 # Remove oh-my-zsh created .zshrc and link our custom one
 ( [ -e ~/.zshrc ] || [ -L ~/.zshrc ] ) && rm ~/.zshrc
 ln -s "$DOTFILES_DIR/modules/zsh/.zshrc" ~/.zshrc
