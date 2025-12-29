673f5ffb229c574e198cde24de3ff9305227635b
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-12-29 22:56:06 +0100

    automated dev commit

--- modules/core/install.sh
+++ modules/core/install.sh
@@ -3,4 +3,15 @@
 set -e
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y make build-essential procps curl libssl-dev zlib1g-dev fzf gcc jq tree 
+sudo apt install -y \
+    make \
+    build-essential  \
+    procps  \
+    curl  \
+    libssl-dev  \
+    zlib1g-dev  \
+    fzf  \
+    gcc  \
+    jq  \
+    tree  \
+    wl-clipboard # Added for nvim clipboard support (otherwise requires poor nvim startup time)
