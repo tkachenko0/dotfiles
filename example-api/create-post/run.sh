ba67b027d62103b0d09fb35cead770f4dd911123
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2026-01-21 12:23:57 +0100

    automated dev commit

--- example-api/create-post/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -s \
-  -X POST \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $AUTH_TOKEN" \
-  -d @"$(dirname "$0")/body.json" \
-  "$BASE_URL/posts" | jq .
--- example-api/get-posts/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -s \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $AUTH_TOKEN" \
-  "$BASE_URL/posts" | jq .
