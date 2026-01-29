38b7502783d41cb357c850a87684850a525ef3b8
v.tkachenko <t.viacheslav00@gmail.com>
2026-01-30 00:40:52 +0100

    automated dev commit

--- modules/kiro/install.sh
+++ modules/kiro/install.sh
@@ -5,11 +5,7 @@ set -e
 curl -fsSL https://cli.kiro.dev/install | bash
 
 kiro-cli settings chat.diffTool "delta \
-  --navigate \
   --side-by-side \
-  --line-numbers \
-  --hyperlinks \
   --syntax-theme 'Visual Studio Dark+' \
-  --hunk-header-style omit \
   --dark"
 
