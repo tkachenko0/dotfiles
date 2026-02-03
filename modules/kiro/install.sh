125406ece2f9b6159e664aaa51be173ea53bd515
v.tkachenko <viacheslav.tkachenko@avvale.com>
2026-02-03 16:13:08 +0100

    fix(kiro)

--- modules/kiro/install.sh
+++ modules/kiro/install.sh
@@ -8,5 +8,5 @@ KIR_DIR=~/.kiro
 
 mkdir -p $KIR_DIR
 
-ln -s "$DOTFILES_DIR/kiro/settings" "$KIR_DIR/settings"
-ln -s "$DOTFILES_DIR/kiro/steering" "$KIR_DIR/steering"
+ln -s "$DOTFILES_DIR/modules/kiro/settings" "$KIR_DIR/settings"
+ln -s "$DOTFILES_DIR/modules/kiro/steering" "$KIR_DIR/steering"
