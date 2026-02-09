4b905405c526531fe3021476fa635fb9d7d1aa42
v.tkachenko <t.viacheslav00@gmail.com>
2026-02-09 11:22:27 +0100

    automated dev commit

--- modules/kiro/install.sh
+++ modules/kiro/install.sh
@@ -8,5 +8,8 @@ KIR_DIR=~/.kiro
 
 mkdir -p $KIR_DIR
 
+rm -rf "$KIR_DIR/settings"
 ln -s "$DOTFILES_DIR/modules/kiro/settings" "$KIR_DIR/settings"
+
+rm -rf "$KIR_DIR/steering"
 ln -s "$DOTFILES_DIR/modules/kiro/steering" "$KIR_DIR/steering"
