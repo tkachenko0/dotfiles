b2971ab5f147a08cf11f278dcdd6143585816a91
v.tkachenko <t.viacheslav00@gmail.com>
2026-02-10 20:29:37 +0100

    feat(treesittter)

--- modules/personal-dir/install.sh
+++ modules/personal-dir/install.sh
@@ -5,4 +5,8 @@ set -e
 mkdir -p ~/dev/personal/todos
 touch ~/dev/personal/todos/todo.md
 
-ln -s "$DOTFILES_DIR/modules/personal-dir/.tmux-sessionizer" ~/dev/personal/.tmux-sessionizer
+TMUX_SESSIONIZER_FILE=~/dev/personal/.tmux-sessionizer
+
+[ -e $TMUX_SESSIONIZER_FILE ] || [ -L $TMUX_SESSIONIZER_FILE ] && rm $TMUX_SESSIONIZER_FILE
+
+ln -s "$DOTFILES_DIR/modules/personal-dir/.tmux-sessionizer" $TMUX_SESSIONIZER_FILE
