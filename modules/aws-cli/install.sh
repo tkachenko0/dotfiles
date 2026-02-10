2ff6cf100d83c03aba182f1f6aed203f1753c720
v.tkachenko <viacheslav.tkachenko@avvale.com>
2026-02-10 13:28:04 +0100

    feat(treesittter)

---
+++ modules/aws-cli/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#unzip awscliv2.zip
+#sudo ./aws/install --update
+#rm awscliv2.zip
+#aws --version
