e5d17f7753cd062b1b693ec4ca1069868f6792b8
v.tkachenko <t.viacheslav00@gmail.com>
2026-03-27 22:23:49 +0100

    automated dev commit

--- modules/neovim/install.sh
+++ modules/neovim/install.sh
@@ -11,8 +11,5 @@ git clone git@github.com:tkachenko0/nvim.git "$DOTFILES_DIR/../nvim"
 mkdir -p ~/.config
 ln -s "$DOTFILES_DIR/../nvim" ~/.config/nvim
 
-mkdir -p ~/.local/share/nvim/site/spell
-curl -fLo ~/.local/share/nvim/site/spell/it.utf-8.spl "https://ftp.nluug.nl/pub/vim/runtime/spell/it.utf-8.spl"
-
 mkdir -p ~/dev/databases
 touch ~/dev/databases/connections.json
