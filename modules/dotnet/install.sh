7e9d91dbb3c4524784360e1c7ca8c5b566ea7a76
v.tkachenko <t.viacheslav00@gmail.com>
2026-04-01 18:35:10 +0200

    automated dev commit

--- modules/dotnet/install.sh
+++ modules/dotnet/install.sh
@@ -2,17 +2,15 @@
 
 set -e
 
-DOTNET_VERSION="${DOTNET_VERSION:-8.0.400}"
+DOTNET_CHANNELS=("8.0" "10.0")
 DOTNET_DIR="$HOME/.dotnet"
 
-if [ -d "$DOTNET_DIR" ]; then
-    echo ".NET already installed at $DOTNET_DIR"
-    exit 0
-fi
-
-echo "Installing .NET SDK $DOTNET_VERSION..."
-
-wget https://dot.net/v1/dotnet-install.sh -O /tmp/dotnet-install.sh
+wget -q https://dot.net/v1/dotnet-install.sh -O /tmp/dotnet-install.sh
 chmod +x /tmp/dotnet-install.sh
-/tmp/dotnet-install.sh --version "$DOTNET_VERSION" --install-dir "$DOTNET_DIR"
-rm /tmp/dotnet-install.sh
+
+for channel in "${DOTNET_CHANNELS[@]}"; do
+    echo "Installing .NET SDK (channel $channel)..."
+    /tmp/dotnet-install.sh --channel "$channel" --install-dir "$DOTNET_DIR"
+done
+
+rm -f /tmp/dotnet-install.sh
